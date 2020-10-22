95af03a8b8dbbfecb8ea6570b93cec0b39ede9fa
rifatabrarjowad <rajdiprifat2004@gmail.com>
2020-10-23 01:10:12 +0600

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,22 @@
+<nav  class="navbar navbar-expand-lg navbar-primary bg-primary">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a style="color:white !important" class="navbar-brand" href="#">E-com</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a style="color:white !important" class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a style="color:white !important" class="nav-link" href="#">Order</a>
+      </li>
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input  class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button style="color:white !important" class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <a style="color:white !important" class="navbar-brand btn-outline-success my-2 my-sm-0 p-4" href="#" >ADD to CArt</a>
+  </div>
+</nav>
